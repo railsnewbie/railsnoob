bc8a0a27c48c3eefd2cc71c1f409e2b6f1c62e8b
RailsNewbie <railsnoob@gmail.com>
2010-04-26 01:04:04 -0400

    MySQL database setup

--- config/initializers/new_rails_defaults.rb
+++ config/initializers/new_rails_defaults.rb
@@ -4,11 +4,11 @@
 # for Rails 3. You can remove this initializer when Rails 3 is released.
 
 if defined?(ActiveRecord)
-  # Include Active Record class name as root for JSON serialized output.
-  ActiveRecord::Base.include_root_in_json = true
+	# Include Active Record class name as root for JSON serialized output.
+	ActiveRecord::Base.include_root_in_json = true
 
-  # Store the full class name (including module namespace) in STI type column.
-  ActiveRecord::Base.store_full_sti_class = true
+	# Store the full class name (including module namespace) in STI type column.
+	ActiveRecord::Base.store_full_sti_class = true
 end
 
 ActionController::Routing.generate_best_match = false
